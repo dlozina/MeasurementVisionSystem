d3c4cb8af820935d0d0abef224f8e311a2e389e7
dlozina <dino.lozina@live.com>
2017-11-10 13:35:24 +0100

    InitalCommit
    Whole Project local Commit to GitHub

---
+++ MjerniStolLimovi/MjerniStolLimovi/A.xaml.cs
@@ -0,0 +1,296 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Microsoft.Expression.Shapes;
+
+namespace MjerniStolLimovi
+{
+	/// <summary>
+	/// Interaction logic for A.xaml
+	/// </summary>
+	public partial class A : UserControl , ILimKontrola
+	{
+        List<Arc> colorPointList = new List<Arc>();
+        List<object> pointList = new List<object>();
+        List<Point> pointListTranslatedAndRotated = new List<Point>();
+        
+        public Grid Circles
+        {
+            get { return circles; }
+            set { circles = value; }
+        }
+
+        public Grid MainLines
+        {
+            get { return mainLines; }
+            set { mainLines = value; }
+        }
+
+        public Grid PointArcs
+        {
+            get { return pointArcs; }
+            set { pointArcs = value; }
+        }
+
+        public Grid CircleArcs
+        {
+            get { return circleArcs; }
+            set { circleArcs = value; }
+        }
+
+        public Grid Centerlines
+        {
+            get { return centerlines; }
+            set { centerlines = value; }
+        }
+
+        public List<string> MeasuresList
+        {
+            get 
+            {
+                List<string> measTemp = new List<string>();
+                foreach (string s in measuresArray.Items)
+                {
+                    measTemp.Add(s);
+                }
+                return measTemp; 
+            }
+        }
+        
+        int purpose = 0;
+        public int Purpose
+        {
+            get { return purpose; }
+            set 
+            { 
+                purpose = value;
+                LimoviCommon.purposeLookRefresh(purpose, pointArcs, lines, lineArrows, dimensionNames, lineEnumeration, circleEnumeration, angleEnumeration, circleArcs, centerlines);
+            }
+        }
+
+        public List<object> PointList
+        {
+            get { return pointList; }
+            set
+            {
+                pointList = value;
+                LimoviCommon.pointColoring(PointList, colorPointList, Purpose);
+                if (PointList.Count >= colorPointList.Count)
+                {
+                    pointListTranslatedAndRotated = LimoviCommon.translateAndRotate(PointList, (Point)PointList[10], (Point)PointList[0]);
+                    var tempList = GetAll();
+                }
+            }
+        }
+
+        public String SheetName
+        {
+            get { return TextBlockA.Text; }
+        }
+
+        public A()
+        {
+            this.InitializeComponent();
+            colorPointList.Add(Point1);
+            colorPointList.Add(Point2);
+            colorPointList.Add(Point3);
+            colorPointList.Add(Point4);
+            colorPointList.Add(Point5);
+            colorPointList.Add(Point6);
+            colorPointList.Add(Point7);
+            colorPointList.Add(Point8);
+            colorPointList.Add(Point9);
+            colorPointList.Add(Point10);
+            colorPointList.Add(Point11);
+            LimoviCommon.pointColoring(PointList, colorPointList, Purpose);
+        }
+
+        //double GetOne(int measureNumber)
+        //{
+        //    double centerline2X, centerline1X;
+        //    switch (measureNumber)
+        //    {
+        //        case 0: // B
+        //            return (Math.Abs(pointListTranslatedAndRotated[5].Y) + Math.Abs(pointListTranslatedAndRotated[4].Y) + Math.Abs(pointListTranslatedAndRotated[2].Y) + Math.Abs(pointListTranslatedAndRotated[1].Y)) / 4;
+        //        case 1: // L4 BOTTOM
+        //            centerline2X = Math.Abs(pointListTranslatedAndRotated[6].X + pointListTranslatedAndRotated[5].X) / 2;
+        //            return Math.Abs(pointListTranslatedAndRotated[10].X - centerline2X);
+        //        case 2: // L5
+        //            return Math.Abs(pointListTranslatedAndRotated[9].X - pointListTranslatedAndRotated[10].X);
+        //        case 3: // L6
+        //            return Math.Abs(pointListTranslatedAndRotated[3].X - pointListTranslatedAndRotated[9].X);
+        //        case 4: // L3
+        //            return Math.Abs(pointListTranslatedAndRotated[8].X - pointListTranslatedAndRotated[3].X);
+        //        case 5: // L2
+        //            return Math.Abs(pointListTranslatedAndRotated[7].X - pointListTranslatedAndRotated[8].X);
+        //        case 6: // L1
+        //            centerline1X = Math.Abs(pointListTranslatedAndRotated[1].X + pointListTranslatedAndRotated[0].X) / 2;
+        //            return Math.Abs(centerline1X - pointListTranslatedAndRotated[7].X);
+        //        default:
+        //            return 0;
+        //    }
+        //}
+
+        double GetOne(int measureNumber)
+        {
+            double centerline2X, centerline1X;
+            switch (measureNumber)
+            {
+                case 0: // B
+                    return (Math.Abs(pointListTranslatedAndRotated[9].Y) + Math.Abs(pointListTranslatedAndRotated[6].Y) + Math.Abs(pointListTranslatedAndRotated[4].Y) + Math.Abs(pointListTranslatedAndRotated[1].Y)) / 4;
+                case 3: // L4 BOTTOM ok
+                    centerline1X = Math.Abs(pointListTranslatedAndRotated[1].X + pointListTranslatedAndRotated[0].X) / 2;
+                    return Math.Abs(centerline1X - pointListTranslatedAndRotated[2].X);
+                case 2: // L5 ok
+                    return Math.Abs(pointListTranslatedAndRotated[2].X - pointListTranslatedAndRotated[3].X);
+                case 1: // L6 ok
+                    return Math.Abs(pointListTranslatedAndRotated[3].X - pointListTranslatedAndRotated[5].X);
+                case 6: // L3 ok
+                    return Math.Abs(pointListTranslatedAndRotated[5].X - pointListTranslatedAndRotated[7].X);
+                case 5: // L2 ok
+                    return Math.Abs(pointListTranslatedAndRotated[7].X - pointListTranslatedAndRotated[8].X);
+                case 4: // L1 ok
+                    centerline2X = Math.Abs(pointListTranslatedAndRotated[10].X + pointListTranslatedAndRotated[9].X) / 2;
+                    return Math.Abs(pointListTranslatedAndRotated[8].X - centerline2X);
+                case 7:
+                    return (GetOne(1) + GetOne(2) + GetOne(3) + GetOne(4) + GetOne(5) + GetOne(6));
+                default:
+                    return 0;
+            }
+        }
+
+        public List<double> GetAll()
+        {
+            var tempList = new List<double>();
+            for (int i = 0; i < measuresArray.Items.Count; i++)
+            {
+                tempList.Add(GetOne(i));
+            }
+            return tempList;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            PointList.Add(new Point(11.2, 1.6));
+            PointList = PointList;
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            PointList.Add(null);
+            PointList = PointList;
+        }
+
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+            PointList = new List<object>();
+            PointList = PointList;
+        }
+
+        private void Button_Click_3(object sender, RoutedEventArgs e)
+        {
+            PointList = PointList;
+        }
+
+        private void Button_Click_4(object sender, RoutedEventArgs e)
+        {
+           
+        }
+
+        private void Button_Click_5(object sender, RoutedEventArgs e)
+        {
+            if (PointList.Count < 14)
+            {
+                PointList.Add(new Point(99,36));
+                PointList.Add(new Point(84,15.5));
+                PointList.Add(new Point(61.2,11.5));
+                PointList.Add(new Point(51.3,18.5));
+                PointList.Add(new Point(44,8.5));
+                PointList.Add(new Point(21,5));
+                PointList.Add(new Point(0.5,19.5));
+                PointList.Add(new Point(79,24));
+                PointList.Add(new Point(71.2,23));
+                PointList.Add(new Point(63.5,21.3));
+                PointList.Add(new Point(39,17.2));
+                PointList.Add(new Point(30.8,16));
+                PointList.Add(new Point(23,14.5));
+                PointList = PointList;
+            }
+        }
+
+        /*
+        if (PointList.Count < 14)
+            {
+                PointList.Add(new Point());
+                PointList.Add(new Point());
+                PointList.Add(new Point());
+                PointList.Add(new Point());
+                PointList.Add(new Point());
+                PointList.Add(new Point());
+                PointList.Add(new Point());
+                PointList.Add(new Point());
+                PointList.Add(new Point());
+                PointList.Add(new Point());
+                PointList.Add(new Point());
+                PointList.Add(new Point());
+                PointList.Add(new Point());
+                PointList = PointList;
+            }
+         */
+
+
+        #region userControl_MouseDown
+        /*
+        private void UserControl_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            Path tempLine;
+            Ellipse tempCircle;
+            
+            if (Mouse.DirectlyOver is Path)
+            {
+                tempLine = ((Path)Mouse.DirectlyOver);
+                if (tempLine.Name.Substring(0,4).Contains("Line"))
+                {
+                    for (int i = 0; i < mainLines.Children.Count; i++)
+                    {
+                        ((Path)mainLines.Children[i]).Stroke = Brushes.Black;
+                    }
+                    for (int i = 0; i < Circles.Children.Count; i++)
+                    {
+                        ((Ellipse)Circles.Children[i]).Stroke = Brushes.Black;
+                    }
+                    tempLine.Stroke = Brushes.OrangeRed;
+                }
+            }
+
+            if (Mouse.DirectlyOver is Ellipse)
+            {
+                tempCircle = ((Ellipse)Mouse.DirectlyOver);
+                if (tempCircle.Name.Substring(0, 6).Contains("Circle"))
+                {
+                    for (int i = 0; i < mainLines.Children.Count; i++)
+                    {
+                        ((Path)mainLines.Children[i]).Stroke = Brushes.Black;
+                    }
+                    for (int i = 0; i < Circles.Children.Count; i++)
+                    {
+                        ((Ellipse)Circles.Children[i]).Stroke = Brushes.Black;
+                    }
+                    tempCircle.Stroke = Brushes.OrangeRed;
+                }
+            }
+        }
+        */
+        #endregion
+
+    }
+}
---
+++ Vizijski_Omco_WPF/App.xaml.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Windows.Shapes;
+using System.Windows.Media;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Threading;
+using HalconDotNet;
+
+
+namespace VizijskiSustavWPF
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        public static PIzvjestaji pIzvjestaji;
+        public static PPostavke pPostavke;
+        public static PDimenzije pDimenzije; 
+        public static PSrh pSrh;
+        public static PValovitost pValovitost;
+        public static PSablja pSablja;
+        public static PKut pKut;
+        public static PRucno pRucno;
+        public static PLCInterface PLC;
+        public static string  ReportPath = "reports/ControlSheet.xlsx";
+        public static MainWindow mwHandle;
+        public static ReportInterface MainReportInterface;
+        public static Algoritmi AutoSearch = new Algoritmi();
+        public static HDevelopExport HDevExp;
+        private bool edgeDetection=false;
+
+
+
+        public App()
+        {
+            System.Diagnostics.Process.GetCurrentProcess().PriorityClass = System.Diagnostics.ProcessPriorityClass.High;
+            InitializeComponent();
+            MainReportInterface = ((ReportInterface)Application.Current.FindResource("MainReport"));
+           
+            PLC = ((PLCInterface)Application.Current.FindResource("PLCinterf"));
+            pIzvjestaji = new PIzvjestaji();
+            pPostavke = new PPostavke();
+            pDimenzije = new PDimenzije();
+            pSrh = new PSrh();
+            pValovitost = new PValovitost();
+            pSablja = new PSablja();
+            pKut = new PKut();
+            pRucno = new PRucno();
+            pRucno = new PRucno();
+
+            App.PLC.StartCyclic();
+            App.PLC.Update_Online_Flag += new PLCInterface.OnlineMarker(PLCInterface_PLCOnlineChanged);
+            App.PLC.Update_100_ms += new PLCInterface.UpdateHandler(PLC_Update_100_ms);
+            App.HDevExp.UpdateResult += new HDevelopExport.UpdateHandler(HalconUpdate);
+            
+
+        }
+
+        private void PLC_Update_100_ms(PLCInterface sender, PLCInterfaceEventArgs e)
+        {
+            String msg = "";
+
+            // Start analize slike PRVOG RUBA S1
+            if (((bool)e.StatusData.Kamere.CAM4ZahtjevZaAnalizomS1.Value) && (!edgeDetection)) //Edge detection
+            {
+
+                HDevExp.InitHalcon();
+                Thread exportThread = new Thread(new ThreadStart(this.RunDia1));
+                exportThread.Start();
+            }
+            edgeDetection = (bool)e.StatusData.Kamere.CAM4ZahtjevZaAnalizomS1.Value == true; //Edge detection help marker
+
+            // Start analize slike PRVOG RUBA S2
+            if (((bool)e.StatusData.Kamere.CAM4ZahtjevZaAnalizomS2.Value) && (!edgeDetection))
+            {
+
+                HDevExp.InitHalcon();
+                Thread exportThread = new Thread(new ThreadStart(this.RunDia2));
+                exportThread.Start();
+            }
+            edgeDetection = (bool)e.StatusData.Kamere.CAM4ZahtjevZaAnalizomS2.Value == true;
+
+            // Prioritet poruka (najviši prioritet je na dnu)
+            //if ((bool)e.StatusData.RotacijskaOs.AutomaticActive.Value) msg = "MJERENJE SRHA U TIJEKU";
+            //if ((bool)e.StatusData.VertikalnaOs.AutomaticActive.Value) msg = "MJERENJE VALOVITOSTI U TIJEKU";
+            //if ((bool)e.StatusData.Ticalo.AutomaticActive.Value) msg = "MJERENJE DIMENZIJA U TIJEKU";
+            //if ((bool)e.StatusData.HorizontalnaOs.ReferencedX.Value == false) msg = "OS X NIJE REFERENCIRANA";
+            //if ((bool)e.StatusData.HorizontalnaOs.ReferencedY.Value == false) msg = "OS Y NIJE REFERENCIRANA";
+            //if ((bool)e.StatusData.HorizontalnaOs.FaultX.Value == true) msg = "GREŠKA OSI X";
+            //if ((bool)e.StatusData.HorizontalnaOs.FaultY.Value == true) msg = "GREŠKA OSI Y";
+            if (mwHandle != null)
+            { 
+            mwHandle.tb_statusMessage.Dispatcher.BeginInvoke((Action)(() => { mwHandle.tb_statusMessage.Text = msg; }));
+            }
+
+            
+        }
+
+        private void RunDia1()
+        {
+
+            HDevExp.RunHalconS1();
+
+        }
+
+        private void RunDia2()
+        {
+
+            HDevExp.RunHalconS2();
+
+        }
+
+        private void HalconUpdate(HDevelopExport sender, HalconEventArgs e)
+        {
+            App.PLC.WriteTag(PLC.STATUS.Kamere.CAM4Rezultat, e.PXvalue);
+            App.PLC.WriteTag(PLC.STATUS.Kamere.CAM4AnalizaOk, true);
+            App.PLC.WriteTag(PLC.STATUS.Kamere.CAM4AnalizaOk, false);
+        }
+
+
+        // Event handler koji se poziva kad PLC postane online ili offline (Ethernet kabel se spoji ili odspoji).
+        private void PLCInterface_PLCOnlineChanged(object sender, OnlineMarkerEventArgs e)
+        {
+            if (e.OnlineMark)
+            {
+                mwHandle.onlineFlag.Dispatcher.BeginInvoke((Action)(() => {mwHandle.onlineFlag.Fill = new LinearGradientBrush(Colors.Green, Colors.White, 0.0); }));
+                mwHandle.t_connectionStatus.Dispatcher.BeginInvoke((Action)(() => { mwHandle.t_connectionStatus.Text = "PLC Status: Online"; }));
+            }
+            else
+            {
+                mwHandle.onlineFlag.Dispatcher.BeginInvoke((Action)(() => { mwHandle.onlineFlag.Fill = new LinearGradientBrush((Color)ColorConverter.ConvertFromString("#FF979797"), Colors.White, 0.0); }));
+                mwHandle.t_connectionStatus.Dispatcher.BeginInvoke((Action)(() => { mwHandle.t_connectionStatus.Text = "PLC Status: Offline"; }));
+            }
+        }
+
+    }
+}
